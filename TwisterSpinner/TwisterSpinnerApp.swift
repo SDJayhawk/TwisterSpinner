bb879bedbb20b2d7484e5dff0c5148a092bc0c5e
scr1967 <scr@sdroses.net>
2025-08-18 13:58:59 -0700

    Initial Commit

---
+++ TwisterSpinner/TwisterSpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwisterSpinnerApp.swift
+//  TwisterSpinner
+//
+//  Created by Steve Rose on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TwisterSpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
